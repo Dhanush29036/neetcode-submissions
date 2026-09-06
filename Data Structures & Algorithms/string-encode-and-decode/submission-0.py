b66f3297155d19204522b43123456e444b832181
Dhanush29036 <134727078+Dhanush29036@users.noreply.github.com>
2026-09-06 11:15:28 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    ans=[]
+    def encode(self, strs: List[str]) -> str:
+        self.ans=strs
+        return ""
+    def decode(self, s: str) -> List[str]:
+        return self.ans
