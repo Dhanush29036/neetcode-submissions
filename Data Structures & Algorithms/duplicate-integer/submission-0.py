28e18a50b2be313bfa0fe64d47e0de68f772ea2c
Dhanush29036 <134727078+Dhanush29036@users.noreply.github.com>
2026-09-06 09:13:36 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        d=defaultdict(int)
+        for i in nums:
+            if d[i]!=0:
+                return True
+            d[i]+=1
+        return False
